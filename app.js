29a351f11c5461e6246e1c5f555e2a4922007b49
JRob <72467031+Jonathan-84@users.noreply.github.com>
2020-12-14 23:55:36 -0500

    got it to match module appearance for user input

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+//console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
     // This...
-   /* for (let i = 0; i < profileDataArr.length; i += 1) {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
       console.log(profileDataArr[i]);
     }
   
-    console.log('================');*/
+    console.log('================');
   
     // Is the same as this...
     profileDataArr.forEach(profileItem => console.log(profileItem));
